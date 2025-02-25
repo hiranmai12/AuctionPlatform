f37003d6d3da831797b9ead4bcfc6652c4f52b5b
hiranmai12 <hiranmai871@gmail.com>
2025-02-25 19:36:14 +0530

    Re-added frontend correctly

---
+++ frontend/src/components/Dashboard.jsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
+import { Navigate } from "react-router-dom";
+function Dashboard() {
+  const [items, setItems] = useState([]);
+  const nav = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem('authToken');
+    if (!token) {
+      nav('/signin');
+    }
+
+    const fetchItems = async () => {
+      try {
+        const res = await axios.get('http://localhost:8000/auctions');
+        setItems(res.data);
+      } catch (error) {
+        console.error('Error fetching auctions:', error);
+      }
+    };
+    fetchItems();
+  }, []);
+
+
+
+  return (
+    <div>
+      <h2>Auction Dashboard</h2>
+
+
+      <Link to="/post-auction">
+        <button>Post New Auction</button>
+      </Link>
+
+      <ul>
+        {items.map((item) => (
+          <li key={item._id}>
+            <Link to={`/auction/${item._id}`}>
+              {item.itemName} - Current Bid: ${item.currentBid} {item.isClosed ? '(Closed)' : ''}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Dashboard;
